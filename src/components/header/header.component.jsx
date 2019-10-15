94d6761d969d29681d0c19936ba24249ccd8e324
Cody Chhay <menghengchhay@gmail.com>
2019-10-15 16:56:32 -0700

    add cart-dropdown and implement toggleCartHidden reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import {connect} from 'react-redux';
 
 import CartIcon from '../cart-icon/cart-icon.component'
+import CardDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import {ReactComponent as Logo} from "../../assets/crown.svg"
 
@@ -10,7 +11,8 @@ import './header.styles.scss'
 
 import {auth} from '../../firebase/firebase.utils'
 
-const Header = ({ currentUser }) => (
+// NOTE-- currentUser and hidden are props from redux mapStateToProps
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/' >
                 <Logo className='logo' />
@@ -29,11 +31,19 @@ const Header = ({ currentUser }) => (
                 }
                 <CartIcon />
         </div>
+        { hidden ? null : <CardDropdown /> }
     </div>
 )
-
+// One way-- accept state from redux without destructuring
 const mapStateToProps = state => ({
-        currentUser: state.user.currentUser
+        currentUser: state.user.currentUser,
+        hidden: state.cart.hidden
 });
+
+// Second way-- destructure nested object and using ES6 same name property
+// const mapStateToProps = ({user:{currentUser}, cart: {hidden}}) => ({
+//         currentUser,
+//         hidden
+// });
 
 export default connect(mapStateToProps)(Header);
