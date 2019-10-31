90184824ffd2dbe8f02dcd4f723d6835de01a889
Cody Chhay <menghengchhay@gmail.com>
2019-10-31 15:58:42 -0700

    create new selector to map collections obj to array so we can use .map on it in CollectionOverview component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,13 @@ export const collectionsSelector = createSelector(
     (shop) => shop.collections
 );
 
+// we converted our collections from array to object
+// so we need this selector to map object to array for .map in CollectionOverview 
+export const selectCollectionsForPreview = createSelector(
+    [collectionsSelector],
+    (collections) => Object.keys(collections).map(key => collections[key])
+)
+
 export const collectionSelector = collectionId => 
     createSelector(
         [collectionsSelector],
